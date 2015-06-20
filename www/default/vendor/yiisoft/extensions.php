b012d6d5bda77f84a2a38c668a22ee7266a3587d
dareen <dareen.alhiyari@gmail.com>
2015-06-20 13:18:10 +0400

    fixed oauth2, finally!

--- www/default/vendor/yiisoft/extensions.php
+++ www/default/vendor/yiisoft/extensions.php
@@ -66,4 +66,13 @@ return array (
       '@yii/elasticsearch' => $vendorDir . '/yiisoft/yii2-elasticsearch',
     ),
   ),
+  'filsh/yii2-oauth2-server' => 
+  array (
+    'name' => 'filsh/yii2-oauth2-server',
+    'version' => '9999999-dev',
+    'alias' => 
+    array (
+      '@filsh/yii2/oauth2server' => $vendorDir . '/filsh/yii2-oauth2-server',
+    ),
+  ),
 );
