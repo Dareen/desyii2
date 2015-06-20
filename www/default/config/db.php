a9902203c70872baf4dc11b4e4ab2a7f6e070453
dareen <dareen.alhiyari@gmail.com>
2015-06-20 04:40:17 +0400

    better way to link docker containers

--- www/default/config/db.php
+++ www/default/config/db.php
@@ -2,7 +2,7 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host='.getenv(MYSQL_PORT_3306_TCP_ADDR).';dbname=yii2db',
+    'dsn' => 'mysql:host=mysql;dbname=yii2db',
     'username' => 'admin',
     'password' => 'password',
     'charset' => 'utf8',
