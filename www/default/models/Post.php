b46ff3c78b03a2de6b8bb31a4f8fc8527d313c60
dareen <dareen.alhiyari@gmail.com>
2015-06-20 04:40:03 +0400

    playing around post with elastic search active record

---
+++ www/default/models/Post.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace app\models;
+
+use \yii\elasticsearch\ActiveRecord;
+
+class Post extends ActiveRecord
+{
+    // TODO: Consider PostQuery?
+
+    /**
+     * @return array the list of attributes for this record
+     */
+    public function attributes()
+    {
+        // path mapping for '_id' is setup to field 'id'
+        return [
+            // meta
+            'id',
+            'user_id',
+            'status',
+            # post/listing details
+            # basic
+            'title',
+            'price',
+            'description',
+        ];
+    }
+
+    public static function primaryKey()
+    {
+        return ['id'];
+    }
+
+    /**
+     * Define rules for validation
+     */
+    public function rules()
+    {
+        return [
+            [['title', 'price'], 'required'],
+        ];
+    }
+
+    /**
+     * Defines a scope that modifies the `$query` to return only active(status = 1) posts
+     */
+    public static function active($query)
+    {
+        $query->andWhere(['status' => 1]);
+    }
+}
